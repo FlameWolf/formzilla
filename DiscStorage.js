0cea1c6673fa62c31c8a68de510a3d14cb4e7732
FlameWolf <agnijith@gmail.com>
2022-07-30 17:14:48 +0530

    Add null check for `saveLocation` variable in `process` method of `DiscStorage` class

--- DiscStorage.js
+++ DiscStorage.js
@@ -13,7 +13,7 @@ class DiscStorage {
 		const target = this.target;
 		const file = new FileInternal(name, info);
 		const saveLocation = typeof target === "function" ? target(file) : target;
-		const filePath = path.join(saveLocation.directory || os.tmpdir(), saveLocation.fileName || file.originalName);
+		const filePath = path.join(saveLocation?.directory || os.tmpdir(), saveLocation?.fileName || file.originalName);
 		const fileStream = fs.createWriteStream(filePath);
 		stream.pipe(fileStream);
 		stream.on("close", () => {
