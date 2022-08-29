6fe7514fce6c8496bce37d7cf2c20c288fccc700
FlameWolf <agnijith@gmail.com>
2022-08-29 17:37:32 +0530

    Provide 100% code coverage for `DiscStorage`

--- test/disc-storage.js
+++ test/disc-storage.js
@@ -26,3 +26,31 @@ tap.test("should save file to disc and populate request body", async t => {
 		console.log(err);
 	}
 });
+tap.test("should read file save target from function", async t => {
+	const instance = require("fastify").fastify();
+	t.teardown(async () => {
+		await instance.close();
+	});
+	try {
+		instance.addHook("onResponse", async (request, reply) => {
+			const requestBody = request.body;
+			t.type(requestBody.name, "string");
+			t.type(requestBody.avatar.path, "string");
+			t.type(requestBody.age, "number");
+			t.type(requestBody.address, "object");
+			t.equal(reply.statusCode, 200);
+		});
+		const req = await setup(instance, {
+			storage: new DiscStorage(file => {
+				return {
+					directory: __dirname,
+					fileName: `${Date.now()}_${file.originalName}`
+				};
+			})
+		});
+		const [res] = await once(req, "response");
+		res.resume();
+	} catch (err) {
+		console.log(err);
+	}
+});
