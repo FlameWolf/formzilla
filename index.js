ed4ef3b1cf1730d6c6aba64b48574ebe7a2cbcac
FlameWolf <agnijith@gmail.com>
2024-08-20 14:26:04 +0530

    Refactor code; update package version to `3.2.18`

--- index.js
+++ index.js
@@ -10,30 +10,30 @@ const formDataParser = async (instance, options) => {
 	const { limits, storage = new StreamStorage() } = options;
 	instance.addContentTypeParser("multipart/form-data", (request, message, done) => {
 		const results = [];
-		const body = new Map();
+		const body = Object.create(null);
 		const props = request.routeOptions.schema?.body?.properties;
 		const parser = props ? new FieldParserWithSchema(props) : new FieldParserNoSchema();
 		const bus = busboy({ headers: message.headers, limits, defParamCharset: "utf8" });
 		bus.on("file", (name, stream, info) => {
 			results.push(storage.process(name, stream, info));
-			const fileProp = body.get(name);
+			const fileProp = body[name];
 			if (!fileProp) {
-				body.set(name, JSON.stringify(info));
+				body[name] = JSON.stringify(info);
 				return;
 			}
 			if (Array.isArray(fileProp)) {
 				fileProp.push(JSON.stringify(info));
 				return;
 			}
-			body.set(name, [fileProp, JSON.stringify(info)]);
+			body[name] = [fileProp, JSON.stringify(info)];
 		});
 		bus.on("field", (name, value) => {
-			body.set(name, parser.parseField(name, value));
+			body[name] = parser.parseField(name, value);
 		});
 		finished(bus, (err = null) => {
 			Promise.all(results).then(files => {
 				request.__files__ = files;
-				done(err, Object.fromEntries(body));
+				done(err, body);
 			});
 		});
 		message.pipe(bus);
@@ -41,22 +41,21 @@ const formDataParser = async (instance, options) => {
 	instance.addHook("preHandler", async request => {
 		const files = request.__files__;
 		if (files?.length) {
-			const fileFields = new Map();
+			const fileFields = Object.create(null);
 			for (const file of files) {
 				const field = file.field;
-				delete file.field;
-				const fileProp = fileFields.get(field);
+				const fileProp = fileFields[field];
 				if (!fileProp) {
-					fileFields.set(field, file);
+					fileFields[field] = file;
 					continue;
 				}
 				if (Array.isArray(fileProp)) {
 					fileProp.push(file);
 					continue;
 				}
-				fileFields.set(field, [fileProp, file]);
+				fileFields[field] = [fileProp, file];
 			}
-			Object.assign(request.body, Object.fromEntries(fileFields));
+			Object.assign(request.body, fileFields);
 		}
 		delete request.__files__;
 	});
